f5ed835a42045a8e9e45ab177f8be8bc0db20036
Matthew Dahl <dahl.matthew@gmail.com>
2016-08-03 15:27:24 -0700

    Use latest bunsen-core

--- blueprints/ember-bunsen-core/index.js
+++ blueprints/ember-bunsen-core/index.js
@@ -1,14 +1,15 @@
 module.exports = {
   afterInstall: function () {
     return this.addPackagesToProject([
-      {name: 'bunsen-core', target: '>=0.6.0 <= 2.0.0'}
+      {name: 'bunsen-core', target: '>=0.9.0 <= 2.0.0'}
     ])
       .then(() => {
         return this.addAddonsToProject({
           packages: [
             {name: 'ember-cli-moment-shim', target: '^2.0.0'},
-            {name: 'ember-lodash-shim', target: '0.1.2'},
-            {name: 'ember-z-schema', target: '>=0.1.2 <=2.0.0'}
+            {name: 'ember-lodash-shim', target: '0.1.3'},
+            {name: 'ember-seamless-immutable-shim', target: '0.2.2'},
+            {name: 'ember-z-schema', target: '0.2.4'}
           ]
         })
       })
