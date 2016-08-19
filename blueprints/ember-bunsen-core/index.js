8e99b6a133f785e72c4d3bca4533a84103239308
Matthew Dahl <dahl.matthew@gmail.com>
2016-08-19 12:27:53 -0700

    Update dependencies

--- blueprints/ember-bunsen-core/index.js
+++ blueprints/ember-bunsen-core/index.js
@@ -1,15 +1,15 @@
 module.exports = {
   afterInstall: function () {
     return this.addPackagesToProject([
-      {name: 'bunsen-core', target: '>=0.11.1 <= 2.0.0'}
+      {name: 'bunsen-core', target: '0.12.0'}
     ])
       .then(() => {
         return this.addAddonsToProject({
           packages: [
             {name: 'ember-cli-moment-shim', target: '^2.0.0'},
-            {name: 'ember-lodash-shim', target: '0.1.18'},
-            {name: 'ember-seamless-immutable-shim', target: '0.2.13'},
-            {name: 'ember-z-schema', target: '0.2.33'}
+            {name: 'ember-lodash-shim', target: '^1.0.0'},
+            {name: 'ember-seamless-immutable-shim', target: '^1.0.0'},
+            {name: 'ember-z-schema', target: '^1.0.0'}
           ]
         })
       })
