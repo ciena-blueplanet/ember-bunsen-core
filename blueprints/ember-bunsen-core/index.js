6331d98cae9cff498ce8cfccfd36d897a8761071
Sophy Pal <spal@ciena.com>
2016-11-23 10:33:29 -0800

    Updating bunsen-core

--- blueprints/ember-bunsen-core/index.js
+++ blueprints/ember-bunsen-core/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   afterInstall: function () {
     return this.addPackagesToProject([
-      {name: 'bunsen-core', target: '0.16.6'}
+      {name: 'bunsen-core', target: '0.18.1'}
     ])
       .then(() => {
         return this.addAddonsToProject({
