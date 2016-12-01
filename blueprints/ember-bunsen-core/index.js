e034ba7f9bcb136ec90a3ddc4f07e1c248635760
Matthew Dahl <mdahl@ciena.com>
2016-12-01 06:38:18 -0800

    Add IPv6 formats

--- blueprints/ember-bunsen-core/index.js
+++ blueprints/ember-bunsen-core/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   afterInstall: function () {
     return this.addPackagesToProject([
-      {name: 'bunsen-core', target: '0.18.3'}
+      {name: 'bunsen-core', target: '0.19.1'}
     ])
       .then(() => {
         return this.addAddonsToProject({
