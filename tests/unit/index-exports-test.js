77ccdecd78234d4162ba54301f6570d01c9a55ba
Matthew Dahl <dahl.matthew@gmail.com>
2016-12-04 13:16:34 -0800

    Upgrade bunsen-core

--- tests/unit/index-exports-test.js
+++ tests/unit/index-exports-test.js
@@ -2,9 +2,16 @@ import {expect} from 'chai'
 import {describe, it} from 'mocha'
 
 import {
+  actions,
+  changeUtils,
   dereference,
+  generateView,
+  normalizeView,
+  reducer,
   utils,
-  validator
+  validateModel,
+  validateView,
+  viewV1ToV2
 } from 'bunsen-core'
 
 describe('bunsen-core', function () {
@@ -12,11 +19,39 @@ describe('bunsen-core', function () {
     expect(typeof dereference).to.equal('object')
   })
 
+  it('exports actions', function () {
+    expect(typeof actions).to.equal('object')
+  })
+
+  it('exports changeUtils', function () {
+    expect(typeof changeUtils).to.equal('object')
+  })
+
+  it('exports generateView', function () {
+    expect(typeof generateView).to.equal('function')
+  })
+
+  it('exports normalizeView', function () {
+    expect(typeof normalizeView).to.equal('function')
+  })
+
+  it('exports reducer', function () {
+    expect(typeof reducer).to.equal('function')
+  })
+
   it('exports utils', function () {
     expect(typeof utils).to.equal('object')
   })
 
-  it('exports validator', function () {
-    expect(typeof validator).to.equal('function')
+  it('exports viewV1ToV2', function () {
+    expect(typeof viewV1ToV2).to.equal('function')
+  })
+
+  it('exports validateModel', function () {
+    expect(typeof validateModel).to.equal('function')
+  })
+
+  it('exports validateView', function () {
+    expect(typeof validateView).to.equal('function')
   })
 })
