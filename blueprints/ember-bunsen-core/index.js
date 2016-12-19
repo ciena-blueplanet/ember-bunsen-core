f90e972a11d35b40dd4f61509c125d68a2b468c6
Matthew Dahl <dahl.matthew@gmail.com>
2016-12-19 10:36:18 -0800

    Update bunsen-core

--- blueprints/ember-bunsen-core/index.js
+++ blueprints/ember-bunsen-core/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   afterInstall: function () {
     return this.addPackagesToProject([
-      {name: 'bunsen-core', target: '0.21.0'}
+      {name: 'bunsen-core', target: '0.21.2'}
     ])
       .then(() => {
         return this.addAddonsToProject({
