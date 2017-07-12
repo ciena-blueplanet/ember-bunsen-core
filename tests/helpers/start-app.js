766e8c3480365f5b18f7929acdf2bb35a57ae745
Michael Carroll <juwara0@users.noreply.github.com>
2017-07-12 16:04:42 -0500

    upgrade to EmberCLI_2-12-3

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,20 +1,16 @@
 import Ember from 'ember'
 const {merge, run} = Ember
-
 import Application from '../../app'
 import config from '../../config/environment'
 
 export default function startApp (attrs) {
-  let application
-
   let attributes = merge({}, config.APP)
   attributes = merge(attributes, attrs) // use defaults, but you can override;
 
-  run(() => {
-    application = Application.create(attributes)
+  return run(() => {
+    let application = Application.create(attributes)
     application.setupForTesting()
     application.injectTestHelpers()
+    return application
   })
-
-  return application
 }
